8d77d7c8d7c85131509af5e67afa0ff95d908148
Saad Ahmed <ahmed.saad068@gmail.com>
2023-10-21 15:29:32 +0500

    Commiting three files

--- 02 create.js
+++ 02 create.js
@@ -3,4 +3,36 @@ db.faculty.insertOne({
     Designation : "Tutor/Instructor"
 })
 
+db.faculty.insertMany(
+[{
+    name : "Ayyaz",
+    Designation : "Tutor/Instructor",
+    Department : "Computer Science",
+    course : "Introduction to Programming"
+},
+{
+    name : "Arif",
+    Designation : "Lecturer",
+    Department : "Computer Science",
+    course : "Introduction to Programming"
+},
+{
+    name : "Tayyab",
+    Designation : "Tutor/Instructor",
+    Department : "Computer Science",
+    course : "Introduction to Programming"
+},
+{
+    name : "Ghulam Abbas",
+    Designation : "Tutor/Instructor",
+    Department : "Computer Science",
+    course : "Introduction to Programming"
+},
+{
+    name : "Kamran",
+    Designation : "Tutor/Instructor",
+    Department : "Computer Science",
+    course : "Introduction to Programming"
+}])
+
 
